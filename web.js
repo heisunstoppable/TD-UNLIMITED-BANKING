479f02cdf560e95ea5455cb1d6734d1a543006d1
Temitope S. Ayeni <unstoppable.jony@gmail.com>
2026-08-07 13:41:49 +0100

    Update web.js

--- web.js
+++ web.js
@@ -1,24 +1,24 @@
-// // Change these to your own username and password
-// const correctUser = "Margret@td";
-// const correctPass = "Ivy90@!";
+// Change these to your own username and password
+const correctUser = "Margret@td";
+const correctPass = "Ivy90@!";
 
-// function login() {
-//     const username = document.getElementById('user').value;
-//     const password = document.getElementById('pass').value;
-
-
-//     if (username === correctUser && password === correctPass) {
-//         window.location.href = "welcome.html";
-//     } else {
-//         document.getElementById('result').style.color = 'red';
-//         document.getElementById('result').innerText = 'Invalid credentials!';
-//     }
-// }
+function login() {
+    const username = document.getElementById('user').value;
+    const password = document.getElementById('pass').value;
 
 
-// setTimeout(() => {
-//     window.location.href = "index.html"; 
-//   }, 180000);
+    if (username === correctUser && password === correctPass) {
+        window.location.href = "welcome.html";
+    } else {
+        document.getElementById('result').style.color = 'red';
+        document.getElementById('result').innerText = 'Invalid credentials!';
+    }
+}
+
+
+setTimeout(() => {
+    window.location.href = "index.html"; 
+  }, 180000);
 
 
 
